a99fbcc814ec997d5011fb263753d6e4f3891788
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-11-25 15:04:08 +0000

    Add a start up script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ ! "${LOCAL}" ] || [ ! "${PASSWORD}" ] || [ ! "${DOMAIN}" ] ; then
+    LOCAL="admin"
+    PASSWORD="password"
+    DOMAIN="localhost"
+fi
+
+prosodyctl register $LOCAL $DOMAIN $PASSWORD
+
+prosodyctl start
