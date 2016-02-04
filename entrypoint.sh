8bbb02e228a6194900186be0eee7f0dd769053f5
Matthew Wild <mwild1@gmail.com>
2016-02-04 19:19:17 +0000

    entrypoint.sh: If a command is passed to the image, run prosodyctl instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ "$1" != "prosody" ]]; then
+    exec prosodyctl $*
+    exit 0;
+fi
+
 if [ "$LOCAL" -a  "$PASSWORD" -a "$DOMAIN" ] ; then
     prosodyctl register $LOCAL $DOMAIN $PASSWORD
 fi
