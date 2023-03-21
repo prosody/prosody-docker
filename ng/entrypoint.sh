aa886a0b4965ceb601fe08ac09c14d87afc76eeb
Kim Alvefur <zash@zash.se>
2023-03-21 18:34:20 +0100

    ng: Add FIXME wrt failure to start if data directory owned by root

--- ng/entrypoint.sh
+++ ng/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 data_dir_owner="$(stat -c %u "/var/lib/prosody/")"
 if [[ "$(id -u prosody)" != "$data_dir_owner" ]]; then
+    # FIXME this fails if owned by root
     usermod -u "$data_dir_owner" prosody
 fi
 if [[ "$(stat -c %u /var/run/prosody/)" != "$data_dir_owner" ]]; then
